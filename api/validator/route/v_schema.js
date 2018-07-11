b7268a150aee56a6aa57fac4aee0f132e89f5fe7
tonghuntz <tonghuntz@hotmail.com>
2018-07-11 14:34:03 +0700

    Fixed lib

--- api/validator/route/v_schema.js
+++ api/validator/route/v_schema.js
@@ -5,14 +5,6 @@ const Joi = require('joi');
 //console.log(config.app.hah);
 //console.log(config.db.user);
 
-const schema = Joi.object().keys({
-  username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-  access_token: [Joi.string(), Joi.number()],
-  birthyear: Joi.number().integer().min(1900).max(2013),
-  email: Joi.string().email()
-}).with('username', 'birthyear').without('password', 'access_token');
-
 const _template = {
   "cobrand_inquiry_by_pnnbr": Joi.object().keys({
     partner_id: Joi.string().length(5).required(),
@@ -39,7 +31,6 @@ const _template = {
     mbcode: Joi.string().length(16).required(),
   }),
 }
-
 // /validation/schema/:SCHEMANO
 router.post('/:SCHEMANO', function(req,res){
   console.log('check schema 1');
@@ -51,7 +42,8 @@ router.post('/:SCHEMANO', function(req,res){
   else{
     console.log('check schema 2');
     // do something 
-    let result = Joi.validate(req.body, _template[req.params.SCHEMANO]);
+	console.log();
+    var result = Joi.validate(req.body, _template[req.params.SCHEMANO]);
     if( result.error === null ){
       res.status(200);
       res.end();
