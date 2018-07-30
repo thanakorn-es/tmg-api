ee1d551ee12b04a063a47d387a2bc08be268a289
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2018-07-31 00:10:39 +0700

    Add Test Route iCFS

--- config/endpoint.js
+++ config/endpoint.js
@@ -3,63 +3,71 @@ const config = {
 	{
 		url: '192.168.0.90',
 		protocol: 'http',
-		port: '8090'
+    port: '8090',
+    path: '/icfs/inquiry'
 	},
 	mapp_inquiry:
 	{
 		url: '192.168.0.89',
 		protocol: 'http',
-		port: '8089'
+    port: '8089',
+    path: '/mapp/inquiry'
 	},
 	api_validator:
 	{
 		url: '192.168.0.80',
 		protocol: 'http',
-		port: '8080'
+    port: '8080',
+    path: ''
 	},
 	api_lookup:
 	{
 		url: '192.168.0.81',
 		protocol: 'http',
-		port: '8081'
+    port: '8081',
+    path: ''
 	},
 	api_mcard_inquiry:
 	{
 		url: '192.168.0.82',
 		protocol: 'http',
-		port: '8082'
+    port: '8082',
+    path: ''
 	},
 	api_mcard_command:
 	{
 		url: '192.168.0.83',
 		protocol: 'http',
-		port: '8083'
+    port: '8083',
+    path: ''
 	},
 	api_partner_inquiry:
 	{
 		url: '192.168.0.87',
 		protocol: 'http',
-		port: '8087'
+    port: '8087',
+    path: ''
 	},
 	client_cobrand_inquiry:
 	{
 		url: '192.168.0.84',
 		protocol: 'http',
-		port: '8084'
+    port: '8084',
+    path: ''
 	},
 	client_cobrand_redeem:
 	{
 		url: '192.168.0.85',
 		protocol: 'http',
-		port: '8085'
+    port: '8085',
+    path: ''
 	},
 	client_cobrand_command:
 	{
 		url: '192.168.0.86',
 		protocol: 'http',
-		port: '8086'
+    port: '8086',
+    path: ''
 	}
-	
-	
 };
 module.exports = config;
--- tests/route/icfs.spec.js
+++ tests/route/icfs.spec.js
@@ -1,84 +1,38 @@
-const supertest = require('supertest');
-const chai = require('chai');
-const expect = chai.expect;
 const app = require('../../business/icfs');
-
 const supertest = require('supertest');
-const request = require('request');
+const r = require('request');
+const rp = require('request-promise');
+const request = supertest(app);
+
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const app = require('../../api/mcard/command');
-const request = supertest(app);
 
+const configHost = require('../../config/host');
+const configEndpoint = require('../../config/endpoint');
 
-describe('Register MCard', function(){
+describe('iCFS Inquiry', function(){
+  this.timeout(10000);
+  var stub, responseObject, responseBody;
+
   before(function(){});
   after(function(){});
 
-  context('Functional Test', function(){ 
-    beforeEach(function(){  
-      this.post = sinon.stub(request,'post');
-      const responseObject = {
-        statusCode: 200,
-        headers: {
-          'content-type': 'application/json'
-        }
-      };
-      const responseBody = {
-        status: 'success',
-        data: [
-          {
-            id: 4,
-            name: 'The Land Before Time',
-            genre: 'Fantasy',
-            rating: 7,
-            explicit: false
-          },
-          {
-            id: 5,
-            name: 'Jurassic Park',
-            genre: 'Science Fiction',
-            rating: 9,
-            explicit: true
-          }        
-        ]
-      };
-    });
-    afterEach(function(){
-      // delete IDCard: 1100800302653, MBCODE: xxxxxxxxxxx
-    });
-
-    it('test', function(){
-      supertest
-      const greaterThanTwenty = sinon.stub().returns('something');
-      greaterThanTwenty(0).should.eql('something');
-      
-      expect(true).to.equal(true);
-    });
-    /*
-    it('', function(done){
-      done();
-      expect(res.body.success).to.equal(true);
-      expect(res.statusCode,200);
-
-
-
-      supertest(app)
-      .post('/icfs/inquiry')
-      .send({MBCODE: '7109000900003026'})
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then(function(result){
-        expect(result.fname).is.string();
-        expect(result.lname).is.string();
+  context('Integration Test', function(){    
+    it('should return HTTP status 200', function(done){    
+      request.get({          
+        uri: configHost.protocol + '://' + configHost.url + ':' + configEndpoint.icfs_inquiry.port + configEndpoint.icfs_inquiry.path,
+        resolveWithFullResponse: true
       })
-      .end(function(err, res) {
-        if (err) return done(err);
-          done();
-      });
+      .expect('Content-Type','application/json')
+      .then(function(response){
+        //console.log(response);
+        //console.log(JSON.parse(response.body));
+        //console.log(JSON.parse(response.statusCode));
+        expect(response.statusCode).to.equal(200);
+      })
+      .expect(200, done);
     });
-    */
   });
 });
 
@@ -115,5 +69,79 @@ describe('Inquiry', function() {
          
     });
   });
+});
+*/
+/*
+
+context('Functional Test with Mock', function(){ 
+  //var stub, responseObject, responseBody;
+
+  beforeEach(function(){  
+    stub = sinon.stub(request,'get');
+    responseObject = {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json'
+      }
+    };
+    responseBody = {
+      status: 'success',
+      data: [
+        {
+          fnme: 'TEST',
+          lnme: 'CARD',
+          lgnme: 'TH',
+          mobile: '0991190202'
+        },
+        {
+          fnme: 'TEST',
+          lnme: 'CARD',
+          lgnme: 'TH',
+          mobile: '0991190202'
+        }        
+      ]
+    };
+  });
+  afterEach(function(){
+    // delete IDCard: 1100800302653, MBCODE: xxxxxxxxxxx
+    stub.restore();
+  });
+
+  it('test', function(){
+    stub.yields(null,responseObject, JSON.stringify(responseBody));
+    request.get('/icfs/inquiry', function(err ,res, body){
+      //console.log(res);
+    })
+        .then(function(res){
+       
+      })
+    //const greaterThanTwenty = sinon.stub().returns('something');
+    //greaterThanTwenty(0).should.eql('something');
+    
+    //expect(true).to.equal(true);
+  });
+  
+  it('', function(done){
+    done();
+    expect(res.body.success).to.equal(true);
+    expect(res.statusCode,200);
+
+
+
+    supertest(app)
+    .post('/icfs/inquiry')
+    .send({MBCODE: '7109000900003026'})
+    .set('Accept', 'application/json')
+    .expect(200)
+    .then(function(result){
+      expect(result.fname).is.string();
+      expect(result.lname).is.string();
+    })
+    .end(function(err, res) {
+      if (err) return done(err);
+        done();
+    });
+  });
+  
 });
 */
